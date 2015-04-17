9740506a8b01c6ba94c516ea6ebaddf7fddadc6a
Matt Kennedy <matt.kennedy@datastax.com>
2015-04-17 18:17:09 -0400

    Consolidated configs, added proc mgmt for monitor

--- client/histmon.py
+++ client/histmon.py
@@ -1,11 +1,12 @@
 # histmon.py
 #
 # Follow a file like tail -f.
-# credit to for tail functionality:
+# credit for tail functionality:
 # http://stackoverflow.com/questions/5419888/reading-from-a-frequently-updated-file
 
 import sys
 import os
+import datetime
 import time
 import requests
 import json
@@ -20,17 +21,20 @@ def follow(thefile):
 		yield line
 
 def submit_command(**com):
-	dest_url = "http://localhost:8888"
+	dest_url = os.getenv('RECALL_URL')
 	data = json.dumps(com)
 	r = requests.post(dest_url, data)
+	#TODO - Log failures, recover from lost connection
+	#TODO - tokenize command and scrub against sensitive string hashes
 
 
 if __name__ == '__main__':
 	#http://stackoverflow.com/questions/1111056/get-time-zone-information-of-the-system-in-python
 	tzoffset = (time.timezone if (time.localtime().tm_isdst == 0) else time.altzone) / 60 / 60 * -1
 	histf = os.path.basename(sys.argv[1])
-	hostn = histf[5:-15] # should I pull this from the actual hostname?
-	histd = histf[-14:-4] # should I change this to pull the actual day?
+	hostn=os.getenv('HOSTNAME')
+	#get the UTC time, strip the micros and add zulu flag
+	histd = datetime.datetime.utcnow().isoformat()[:-7]+'Z'
 	logfile = open(sys.argv[1],"r")
 	loglines = follow(logfile)
 	ts = ""
@@ -38,5 +42,4 @@ if __name__ == '__main__':
 		if line[0] == '#':
 			ts = line[1:-1]
 		else:
-			print hostn,histd,ts,tzoffset,line[:-1]
-			submit_command(hostname=hostn, cmddate=histd,cmdts=ts,cmdtz=tzoffset,cmd=line[:-1])
+			submit_command(histfile=histf, hostname=hostn, cmddate=histd,cmdts=ts,cmdtz=tzoffset,cmd=line[:-1])
--- client/recall_conf.sh
+++ client/recall_conf.sh
@@ -1,3 +1,10 @@
+#Configure Recall client settings
+
+RECALL_URL=http://localhost:8888/
+RECALL_MON=$HOME/Documents/recall/client/histmon.py
+RECALL_HISTORY_DIR=$HOME/.recall/histories
+
+#Bash History Config
 #References:
 # http://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows
 # http://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows#3055135
@@ -9,4 +16,10 @@ export HISTIGNORE="ls:ls -l:pwd:cd:cd .."
 export HISTTIMEFORMAT='%b %d %H:%M:%S: '
 shopt -s histappend    # append to history, don't overwrite it
 export PROMPT_COMMAND="history -a; history -c; history -r;"
-export HISTFILE=~/.histories/hist.`echo $HOSTNAME | cut -d. -f1`_`date +%F`.txt
+export HISTFILE=$RECALL_HISTORY_DIR/hist.`echo $HOSTNAME | cut -d. -f1`_`date +%F`.txt
+
+export RECALL_URL
+export HOSTNAME
+python $RECALL_MON $HISTFILE &
+MONPID=$!
+trap 'kill $MONPID' EXIT
